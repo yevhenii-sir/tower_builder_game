13e02b83050083b3ecdc59757e0e03e3ca8bef6b
Yevhenii Sirenko <yevhenii.sirenko@gmail.com>
2022-05-12 21:25:19 +0300

    Added crane movement

--- Main.cs
+++ Main.cs
@@ -3,56 +3,67 @@ using Godot;
 
 public class Main : Node2D
 {
-    //private readonly PackedScene _boxScene = (PackedScene)GD.Load("res://block.tscn");
-    //private int _nodeNameCounter = 0;
-    //private RigidBody2D _currentBlock;
-    private Vector2 _windowSize;
-    
-    private Node2D _ropeNode;
-    private Vector2 _ropeSpriteSize;
-    private float _halfHeightRope;
-    
-    public override void _Ready()
-    {
-        _windowSize = GetViewport().GetVisibleRect().Size;
+	private Vector2 _windowSize;
+	
+	private Node2D _ropeNode;
+	private Vector2 _ropeSpriteSize;
+	private float _halfHeightRope;
 
-        _ropeNode = GetNode<Node2D>("Rope");
-        _ropeSpriteSize = _ropeNode.GetNode<Sprite>("Sprite").Texture.GetSize();
-        _halfHeightRope = _ropeSpriteSize.y / 2;
-    }
+	private static float _deg = 2.5f;
+	private float _tTimeShiftX, _tTimeShiftY;
+	private float _amplitudeX = 100f;
+	private float _amplitudeY = 25f;
+	private float _incrementX = Mathf.Deg2Rad(_deg);
+	private float _incrementY = Mathf.Deg2Rad(_deg) / 2.5f;
 
-    public override void _Process(float delta)
-    {
-        if (OS.GetName() == "Windows" && Input.IsActionPressed("restart_game"))
-            GetTree().ReloadCurrentScene();
+	private Vector2 _topCenterPoint;
 
-        // if (_currentBlock != null)
-        // {
-        //     if (_currentBlock.GetCollidingBodies().Count > 0)
-        //     {
-        //         if (_nodeNameCounter > 1 && !(bool) _currentBlock.Get("SkipBlock"))
-        //         {
-        //             _camera2D.Position = new Vector2(_camera2D.Position.x, _camera2D.Position.y - 196.0f);
-        //             _currentBlock.Set("SkipBlock", true);
-        //         }
-        //     }
-        // }
+	public override void _Ready()
+	{
+		_windowSize = GetViewport().GetVisibleRect().Size;
 
-        FollowRopeToPoint(GetGlobalMousePosition());
-    }
+		_ropeNode = GetNode<Node2D>("Rope");
+		_ropeSpriteSize = _ropeNode.GetNode<Sprite>("Sprite").Texture.GetSize();
+		_halfHeightRope = _ropeSpriteSize.y / 2;
+	}
 
-    private void FollowRopeToPoint(Vector2 trackingPoint)
-    {
-        Vector2 topCenterPoint = new Vector2(_windowSize.x / 2, -GetGlobalTransformWithCanvas().origin.y - 80);
-        float oppositeDirectionToTrackingPoint = (trackingPoint - topCenterPoint).Angle() - Mathf.Deg2Rad(180);
-        float distToTrackingPoint = 
-            Mathf.Clamp(_halfHeightRope - topCenterPoint.DistanceTo(trackingPoint), 0, _halfHeightRope);
-        
-        Vector2 initialOffset = new Vector2((float) Math.Cos(oppositeDirectionToTrackingPoint), 
-            (float) Math.Sin(oppositeDirectionToTrackingPoint)) * distToTrackingPoint;
-        
-        _ropeNode.Position = initialOffset + topCenterPoint;
-        _ropeNode.Rotation = (trackingPoint - topCenterPoint).Angle() - Mathf.Deg2Rad(90);
-    }
+	public override void _Process(float delta)
+	{
+		if (OS.GetName() == "Windows" && Input.IsActionPressed("restart_game"))
+			GetTree().ReloadCurrentScene();
+		
+		_topCenterPoint = new Vector2(_windowSize.x / 2, -GetGlobalTransformWithCanvas().origin.y - 80);
+
+		// if (_currentBlock != null)
+		// {
+		//     if (_currentBlock.GetCollidingBodies().Count > 0)
+		//     {
+		//         if (_nodeNameCounter > 1 && !(bool) _currentBlock.Get("SkipBlock"))
+		//         {
+		//             _camera2D.Position = new Vector2(_camera2D.Position.x, _camera2D.Position.y - 196.0f);
+		//             _currentBlock.Set("SkipBlock", true);
+		//         }
+		//     }
+		// }
+
+		FollowRopeToPoint(_topCenterPoint + new Vector2(_amplitudeX * Mathf.Sin(_tTimeShiftX), 
+														_amplitudeY * Mathf.Sin(_tTimeShiftY) + 300f));
+
+		_tTimeShiftX += _incrementX;
+		_tTimeShiftY += _incrementY;
+	}
+
+	private void FollowRopeToPoint(Vector2 trackingPoint)
+	{
+		float oppositeDirectionToTrackingPoint = (trackingPoint - _topCenterPoint).Angle() - Mathf.Deg2Rad(180);
+		float distToTrackingPoint = 
+			Mathf.Clamp(_halfHeightRope - _topCenterPoint.DistanceTo(trackingPoint), 0, _halfHeightRope);
+		
+		Vector2 initialOffset = new Vector2((float) Math.Cos(oppositeDirectionToTrackingPoint), 
+			(float) Math.Sin(oppositeDirectionToTrackingPoint)) * distToTrackingPoint;
+		
+		_ropeNode.Position = initialOffset + _topCenterPoint;
+		_ropeNode.Rotation = (trackingPoint - _topCenterPoint).Angle() - Mathf.Deg2Rad(90);
+	}
 }
 
--- rope_claws.cs
+++ rope_claws.cs
@@ -5,132 +5,135 @@ using Godot;
 
 namespace TowerBuilder
 {
-    public class rope_claws : Node2D
-    {
-        private Sprite _leftClaw;
-        private Sprite _rightClaw;
-    
-        private float _startRotationRad = 0;
-        private readonly float _finishRotationRad = Mathf.Deg2Rad(45);
-        private float _increaseValAngle = 0.30f;
+	public class rope_claws : Node2D
+	{
+		private Sprite _leftClaw;
+		private Sprite _rightClaw;
+	
+		private float _startRotationRad = 0;
+		private readonly float _finishRotationRad = Mathf.Deg2Rad(45);
+		private float _increaseValAngle = 0.30f;
 
-        private delegate void MoveClaws();
-        private MoveClaws _handlerMoveClaws;
-    
-        private readonly PackedScene _boxScene = (PackedScene)GD.Load("res://block.tscn");
-        private int _nodeNameCounter = 0;
-        private Node2D _currentBox;
+		private delegate void MoveClaws();
+		private MoveClaws _handlerMoveClaws;
+	
+		private readonly PackedScene _boxScene = (PackedScene)GD.Load("res://block.tscn");
+		private int _nodeNameCounter = 0;
+		private Node2D _currentBox;
 
-        private Timer _timerSpawnBox;
+		private Timer _timerSpawnBox;
 
-        public override void _Ready()
-        {
-            _leftClaw = GetNode<Sprite>("claw_left");
-            _rightClaw = GetNode<Sprite>("claw_right");
-            _timerSpawnBox = GetNode<Timer>("timer_box_spawn");
+		public override void _Ready()
+		{
+			_leftClaw = GetNode<Sprite>("claw_left");
+			_rightClaw = GetNode<Sprite>("claw_right");
+			_timerSpawnBox = GetNode<Timer>("timer_box_spawn");
 
-            if (_leftClaw != null && _rightClaw != null && _timerSpawnBox != null)
-                _handlerMoveClaws = MoveClawsDown;
-            else 
-                GetTree().ReloadCurrentScene();
-        }
-    
-        public override void _Process(float delta)
-        {
-            if (_currentBox != null)
-            {
-                if (Input.IsActionJustPressed("mouse_left_pressed"))
-                {
-                    var currentBoxRigidBody2D = _currentBox.GetNode<RigidBody2D>("RigidBody2D");
-                    currentBoxRigidBody2D.Mode = RigidBody2D.ModeEnum.Rigid;
-                
-                    var pos = _currentBox.GlobalPosition;
-                    var angle = _currentBox.GlobalRotation;
-                    RemoveChild(_currentBox);
-                    GetParent().AddChild(_currentBox);
-                    _currentBox.Position = pos;
-                    _currentBox.Rotation = angle;
-                    _currentBox.Scale = new Vector2(1, 1);
-                    _currentBox = null;
-                
-                    _timerSpawnBox.Start();
-                
-                    ClearAllDelegates();
-                    _handlerMoveClaws = MoveClawsUp;
-                }
-                else TrackPositionBox(_currentBox);
-            }
+			if (_leftClaw != null && _rightClaw != null && _timerSpawnBox != null)
+			{
+				_handlerMoveClaws = MoveClawsDown;
+				_on_timer_box_spawn_timeout();
+			}
+			else 
+				GetTree().ReloadCurrentScene();
+		}
+	
+		public override void _Process(float delta)
+		{
+			if (_currentBox != null)
+			{
+				if (Input.IsActionJustPressed("mouse_left_pressed"))
+				{
+					var currentBoxRigidBody2D = _currentBox.GetNode<RigidBody2D>("RigidBody2D");
+					currentBoxRigidBody2D.Mode = RigidBody2D.ModeEnum.Rigid;
+				
+					var pos = _currentBox.GlobalPosition;
+					var angle = _currentBox.GlobalRotation;
+					RemoveChild(_currentBox);
+					GetParent().AddChild(_currentBox);
+					_currentBox.Position = pos;
+					_currentBox.Rotation = angle;
+					_currentBox.Scale = new Vector2(1, 1);
+					_currentBox = null;
+				
+					_timerSpawnBox.Start();
+				
+					ClearAllDelegates();
+					_handlerMoveClaws = MoveClawsUp;
+				}
+				else TrackPositionBox(_currentBox);
+			}
 
-            _handlerMoveClaws?.Invoke();
-        
+			_handlerMoveClaws?.Invoke();
+		
 #if DEBUG
-            if (Input.IsActionJustPressed("claws_up"))
-            {
-                ClearAllDelegates();
-                _handlerMoveClaws = MoveClawsUp;
-            } else if (Input.IsActionJustPressed("claws_down"))
-            {
-                ClearAllDelegates();
-                _handlerMoveClaws = MoveClawsDown;
-            }
+			if (Input.IsActionJustPressed("claws_up"))
+			{
+				ClearAllDelegates();
+				_handlerMoveClaws = MoveClawsUp;
+			} else if (Input.IsActionJustPressed("claws_down"))
+			{
+				ClearAllDelegates();
+				_handlerMoveClaws = MoveClawsDown;
+			}
 #endif
-        }
-    
-        private void MoveClawsUp()
-        {
-            _leftClaw.Rotation = Mathf.LerpAngle(_leftClaw.Rotation, _finishRotationRad, _increaseValAngle);
-            _rightClaw.Rotation = Mathf.LerpAngle(_rightClaw.Rotation, -_finishRotationRad, _increaseValAngle);
+		}
+	
+		private void MoveClawsUp()
+		{
+			_leftClaw.Rotation = Mathf.LerpAngle(_leftClaw.Rotation, _finishRotationRad, _increaseValAngle);
+			_rightClaw.Rotation = Mathf.LerpAngle(_rightClaw.Rotation, -_finishRotationRad, _increaseValAngle);
 
-            if (Math.Abs(_leftClaw.Rotation - _finishRotationRad) < 0.02) _handlerMoveClaws -= MoveClawsUp;
-        }
-    
-        private void MoveClawsDown()
-        {
-            _leftClaw.Rotation = Mathf.LerpAngle(_leftClaw.Rotation, _startRotationRad, _increaseValAngle);
-            _rightClaw.Rotation = Mathf.LerpAngle(_rightClaw.Rotation, _startRotationRad, _increaseValAngle);
-        
-            if (Math.Abs(_leftClaw.Rotation - _startRotationRad) < 0.02) _handlerMoveClaws -= MoveClawsDown;
-        }
-    
-        private void ClearAllDelegates()
-        {
-            if (_handlerMoveClaws != null)
-            {
-                foreach (Delegate dDelegate in _handlerMoveClaws.GetInvocationList())
-                    _handlerMoveClaws -= (MoveClaws) dDelegate;
-            }
-        }
-    
-        private void TrackPositionBox(Node2D box)
-        {
-            box.Position = _leftClaw.Position + new Vector2(0, 40);
-        }
+			if (Math.Abs(_leftClaw.Rotation - _finishRotationRad) < 0.02) _handlerMoveClaws -= MoveClawsUp;
+		}
+	
+		private void MoveClawsDown()
+		{
+			_leftClaw.Rotation = Mathf.LerpAngle(_leftClaw.Rotation, _startRotationRad, _increaseValAngle);
+			_rightClaw.Rotation = Mathf.LerpAngle(_rightClaw.Rotation, _startRotationRad, _increaseValAngle);
+		
+			if (Math.Abs(_leftClaw.Rotation - _startRotationRad) < 0.02) _handlerMoveClaws -= MoveClawsDown;
+		}
+	
+		private void ClearAllDelegates()
+		{
+			if (_handlerMoveClaws != null)
+			{
+				foreach (Delegate dDelegate in _handlerMoveClaws.GetInvocationList())
+					_handlerMoveClaws -= (MoveClaws) dDelegate;
+			}
+		}
+	
+		private void TrackPositionBox(Node2D box)
+		{
+			box.Position = _leftClaw.Position + new Vector2(0, 40);
+		}
 
-        /// <summary>
-        /// Спавн коробки в клещах по окончанию таймера
-        /// </summary>
-        public void _on_timer_box_spawn_timeout()
-        {
-            Node box = _boxScene.Instance();
-            box.Name = "box_" + _nodeNameCounter;
-            AddChild(box);
+		/// <summary>
+		/// Спавн коробки в клещах по окончанию таймера
+		/// </summary>
+		public void _on_timer_box_spawn_timeout()
+		{
+			Node box = _boxScene.Instance();
+			box.Name = "box_" + _nodeNameCounter;
+			AddChild(box);
 
-            _currentBox = GetNode<Node2D>("box_" + _nodeNameCounter);
-            if (_currentBox != null)
-            {
-                TrackPositionBox(_currentBox);
-                _currentBox.GetNode("RigidBody2D").GetNode<Sprite>("Sprite").Scale = new Vector2(0.5f, 0.5f);
-                _currentBox.GetNode("RigidBody2D").GetNode<CollisionShape2D>("CollisionShape2D").Scale =
-                    new Vector2(0.5f, 0.5f);
+			_currentBox = GetNode<Node2D>("box_" + _nodeNameCounter);
+			if (_currentBox != null)
+			{
+				TrackPositionBox(_currentBox);
+				_currentBox.GetNode("RigidBody2D").GetNode<Sprite>("Sprite").Scale = new Vector2(0.5f, 0.5f);
+				_currentBox.GetNode("RigidBody2D").GetNode<CollisionShape2D>("CollisionShape2D").Scale =
+					new Vector2(0.5f, 0.5f);
 
-                var currentBoxRigidBody2D = _currentBox.GetNode<RigidBody2D>("RigidBody2D");
-                currentBoxRigidBody2D.Mode = RigidBody2D.ModeEnum.Static;
-            
-                ClearAllDelegates();
-                _handlerMoveClaws = MoveClawsDown;
-            }
+				var currentBoxRigidBody2D = _currentBox.GetNode<RigidBody2D>("RigidBody2D");
+				currentBoxRigidBody2D.Mode = RigidBody2D.ModeEnum.Static;
+			
+				ClearAllDelegates();
+				_handlerMoveClaws = MoveClawsDown;
+			}
 
-            _nodeNameCounter++;
-        }
-    }
+			_nodeNameCounter++;
+		}
+	}
 }
