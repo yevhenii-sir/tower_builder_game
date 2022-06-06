7da61baaa7d00c7b53332b4fe50465a47de7167d
Yevhenii Sirenko <yevhenii.sirenko@gmail.com>
2022-06-06 13:54:06 +0300

    Added change color

--- interface/SndButton/Area2D.gd
+++ interface/SndButton/Area2D.gd
@@ -3,6 +3,7 @@ extends Area2D
 var animation : AnimationPlayer
 var main : Node2D
 var isPressed = false;
+var onSound = true;
 
 func _ready():
 	animation = get_node("AnimationPlayer");
@@ -16,8 +17,14 @@ func start_back_animation():
 	animation.play_backwards("FadeUp")
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
-	#if !animation.is_playing():
-	if (event is InputEventMouseButton && event.pressed):
-		animation.play("pressAnim");
+	if !animation.is_playing():
+		if (event is InputEventMouseButton && event.pressed):
+			animation.play("pressAnim");
+			if onSound:
+				$background.modulate = Color.lightseagreen;
+				onSound = false
+			else:
+				$background.modulate = Color("f71b1b");
+				onSound = true
 			#main.set("_isStart", false)
 			#main.set("nowPressedPlayBtn", true)
